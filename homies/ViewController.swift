6cf5dd39d39d55b36106fef22a7ab98581a28c2e
vidal francois <vidalfrancois11@gmail.com>
2019-08-29 18:18:28 +0200

    Initial Commit

---
+++ homies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  homies
+//
+//  Created by vidal françois on 29/08/2019.
+//  Copyright © 2019 vidal françois. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
